805bd44c774e9592e577b64d3365ae40e7ded205
Gustavo Ferreira Soares <gustavo.soares@maededeus.com.br>
2018-10-10 11:30:17 -0300

    notificação sobre parada optix

--- intra/inc/CelularPlantao.php
+++ intra/inc/CelularPlantao.php
@@ -2,6 +2,11 @@
     <img src="intra/images/conf.png">
     <h1>Tecnologia da Informação</h1>
     <p>Celular de Plantao da TI - (51) 98600-3639</p>
-    <p>Ambiente de TI Normal</p>
-    <h2 style="color:red">Conheça a nova <a href="<?php echo 'http://'.$_SERVER['SERVER_NAME'].':8080/usuario'?>">Intranet</a> do Gamp!</h2>
+    <!-- <p>Ambiente de TI Normal</p> -->
+    <h2 style="color:blue">PARADA PROGRAMADA</h2>
+    <p style="color:red"> PRESADOS COLABORADORES
+          A PARTIR DAS 00:00 DO DIA 10 ATÉ ÁS 04:00 DO DIA 11 ESTÁ PROGRAMADO  UMA PARADA PARA MANUTENÇÃO NO SISTEMA OPTIX DO HOSPITAL UNIVERSITÁRIO;
+          NESSE PERÍODO, VISUALIZAÇÕES DE IMAGENS DE  EXAMES SOMENTE EMERGENCIALMENTE NOS EQUIPAMENTOS DA RADIOLOGIA CRs E DEMAIS.<br>
+        <p style="font-family:arial"><i>Tecnologia da Informação</i></p>
+    </p>
 </div>
