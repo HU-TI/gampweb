15a72fc42db2cbd3a088a4f362a22e9bf956969f
Gustavo Ferreira Soares <gustavo.soares@maededeus.com.br>
2018-10-11 08:18:08 -0300

    Notificação para erro do sistema MV

--- intra/inc/CelularPlantao.php
+++ intra/inc/CelularPlantao.php
@@ -3,10 +3,10 @@
     <h1>Tecnologia da Informação</h1>
     <p>Celular de Plantao da TI - (51) 98600-3639</p>
     <!-- <p>Ambiente de TI Normal</p> -->
-    <h2 style="color:blue">PARADA PROGRAMADA</h2>
-    <p style="color:red"> PRESADOS COLABORADORES
-          A PARTIR DAS 00:00 DO DIA 10 ATÉ ÁS 04:00 DO DIA 11 ESTÁ PROGRAMADO  UMA PARADA PARA MANUTENÇÃO NO SISTEMA OPTIX DO HOSPITAL UNIVERSITÁRIO;
-          NESSE PERÍODO, VISUALIZAÇÕES DE IMAGENS DE  EXAMES SOMENTE EMERGENCIALMENTE NOS EQUIPAMENTOS DA RADIOLOGIA CRs E DEMAIS.<br>
-        <p style="font-family:arial"><i>Tecnologia da Informação</i></p>
+    <h2 style="color:blue">SISTEMA MV FORA DO AR</h2>
+    <p style="color:red">
+            Sistema MV fora do ar devido a problemas técnicos. <br>Equipe CanoasTec trabalhando na solução.
+            Comunicaremos assim que solucionado.
+            <br><i style="color:blue">Tecnologia da Informação</i>
     </p>
 </div>
