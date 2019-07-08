5d1a8902e156e3197c173917ce169e02366bb2f7
hu-ti <bruno.souza@maededeus.com.br>
2019-07-08 11:44:16 -0300

    criando popup de alerta

--- index.php
+++ index.php
@@ -20,89 +20,9 @@ define('SERVER_OLD_INTRA', "http://".$_SERVER['SERVER_NAME'] );
 	<script src="./intra/js/jquery-2.1.3.min.js"> </script>
 	<link rel="stylesheet" href="./intra/bootstrap-4.3.1/dist/css/bootstrap.min.css">
 	<script src="./intra/bootstrap-4.3.1/dist/js/bootstrap.min.js" ></script>
-	<style>
-		/* The Modal (background) */
-		.modal {
-			display: none; /* Hidden by default */
-			position: fixed; /* Stay in place */
-			z-index: 1; /* Sit on top */
-			padding-top: 100px; /* Location of the box */
-			left: 0;
-			top: 0;
-			width: 100%; /* Full width */
-			height: 100%; /* Full height */
-			overflow: auto; /* Enable scroll if needed */
-			background-color: rgb(0,0,0); /* Fallback color */
-			background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-		}
-
-		/* Modal Content */
-		.modal-content {
-			background-color: #66ccff;
-			margin: auto;
-			padding: 20px;
-			border: 1px solid #888;
-			width: 500;
-		}
-
-		/* The Close Button */
-		.close {
-			color: #aaaaaa;
-			float: right;
-			font-size: 28px;
-			font-weight: bold;
-		}
-
-		.close:hover,
-		.close:focus {
-			color: #000;
-			text-decoration: none;
-			cursor: pointer;
-		}
-		.aviso-titulo {
-			margin: auto;
-		}
-	</style>
 </head>
 	
 <body>
-	<!-- The Modal -->
-	<div id="myModal" class="modal">
-		<!-- Modal content -->
-		<div class="modal-content">
-			<span class="close">&times;</span>	
-			<h1 class="aviso-titulo">Aviso:</h1>
-			<img src="intra/images/comunicado_email.jpg">
-		</div>
-	</div>
-	<script>
-		//Script do popup de avisos
-
-		// Get the modal
-		var modal = document.getElementById("modalAvisos");
-
-		// Get the <span> element that closes the modal
-		var span = document.getElementsByClassName("close")[0];
-
-		// When the user clicks on <span> (x), close the modal
-		span.onclick = function() {
-			modal.style.display = "none";
-		}
-
-		// When the user clicks anywhere outside of the modal, close it
-		window.onclick = function(event) {
-			if (event.target == modal) {
-				modal.style.display = "none";
-			}
-		}
-
-		//Show modal when html loads
-		$(document).ready(function(){
-			if ( window.location.href == "http://localhost/") {
-				modal.style.display = "block";
-			}
-		});
-	</script>
 	<div id="tudo">
 		<div id="navegacao">
 
@@ -1126,6 +1046,43 @@ define('SERVER_OLD_INTRA', "http://".$_SERVER['SERVER_NAME'] );
 			
 		</div>
 	</div>
+	<!-- The Modal -->
+	<div id="modalAvisos" class="main-modal">
+		<!-- Modal content -->
+		<div class="alert-modal-content">
+			<span class="close">&times;</span>	
+			<h1 class="aviso-titulo">Aviso:</h1>
+			<img src="intra/images/comunicado_email.jpg">
+		</div>
+	</div>
+	<script>
+		//Script do popup de avisos
+
+		// Get the modal
+		var modal = document.getElementById("modalAvisos");
+
+		// Get the <span> element that closes the modal
+		var span = document.getElementsByClassName("close")[0];
+
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function() {
+			modal.style.display = "none";
+		}
+
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+			if (event.target == modal) {
+				modal.style.display = "none";
+			}
+		}
+
+		//Show modal when html loads
+		$(document).ready(function(){
+			if ( window.location.href == "http://localhost/") {
+				modal.style.display = "block";
+			}
+		});
+	</script>
 	
 </body>
 <?php
