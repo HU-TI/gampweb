068f256022788c299f230827d011ceb108ee16dc
Murilo Oliveira de Souza <murilo.souza@maededeus.com.br>
2020-08-14 16:06:23 -0300

    troca de imagem de avisos

--- index.php
+++ index.php
@@ -1052,7 +1052,7 @@ define('SERVER_OLD_INTRA', "http://".$_SERVER['SERVER_NAME'] );
 		<div class="alert-modal-content">
 			<span class="close">&times;</span>	
 			<h1 class="aviso-titulo">Aviso:</h1>
-			<img src="intra/images/aviso_arquivos.jpg">
+			<img src="intra/images/covid-ultimo.jpg">
 		</div>
 	</div>
 	<script>
